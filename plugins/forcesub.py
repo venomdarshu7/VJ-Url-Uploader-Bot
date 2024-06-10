e7509cc82626e7053c48c710e84ad91bf9901f2b
venomdarshu7 <138948660+venomdarshu7@users.noreply.github.com>
2024-06-10 13:01:28 +0530

    Update forcesub.py

--- plugins/forcesub.py
+++ plugins/forcesub.py
@@ -34,7 +34,7 @@ async def handle_force_sub(bot: Client, update: Message):
         if user.status == "kicked":
             await bot.send_message(
                 chat_id=update.from_user.id,
-                text="Sorry Sir, You are Banned to use me. Contact my [Support Group](https://t.me/vj_bot_disscussion).",
+                text="Sorry Sir, You are Banned to use me. Contact my [Support Group](https://t.me/chatofvdmovz).",
                 disable_web_page_preview=True
             )
             return 400
@@ -63,7 +63,7 @@ async def handle_force_sub(bot: Client, update: Message):
     except Exception:
         await bot.send_message(
             chat_id=update.from_user.id,
-            text="Something went Wrong. Contact my [Support Group](https://t.me/vj_bot_disscussion).",
+            text="Something went Wrong. Contact my [Support Group](https://t.me/chatofvdmovz).",
             disable_web_page_preview=True
         )
         return 200
